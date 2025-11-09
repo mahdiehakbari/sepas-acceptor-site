35fdaaf8ed98a7ec52282bc35b34ca687d5ccaee
d.karimi <d.karimi@sepasholding.com>
2025-11-09 17:05:25 +0330

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        //destination: 'https://dentalitapi.sepasholding.com/api/:path*',
-        destination: 'http://localhost:3838/api/:path*',
+        destination: 'https://dentalitapi.sepasholding.com/api/:path*',
+        //destination: 'http://localhost:3838/api/:path*',
       },
     ];
   },
