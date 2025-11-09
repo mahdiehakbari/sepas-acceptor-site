1697fa2050eae38ce2497542320bd8cc2266da23
d.karimi <d.karimi@sepasholding.com>
2025-11-09 17:04:45 +0330

    change site title

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://dentalitapi.sepasholding.com/api/:path*',
+        //destination: 'https://dentalitapi.sepasholding.com/api/:path*',
+        destination: 'http://localhost:3838/api/:path*',
       },
     ];
   },
