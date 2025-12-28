c70c75d65fedeb3390811d780b5e118672016021
mahdieh <mahdiehakbari16@gmail.com>
2025-12-28 17:58:09 +0330

    remove proxy

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://dentalitapi.sepasholding.com/api/:path*',
-        //destination: 'http://localhost:3838/api/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://dentalitapi.sepasholding.com/api/:path*',
+  //       //destination: 'http://localhost:3838/api/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
