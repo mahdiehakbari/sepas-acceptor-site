a4cb1681abdff3f6f3c38a1881a8a010efb617ca
mahdieh <mahdiehakbari16@gmail.com>
2025-12-28 18:02:45 +0330

    remove proxy

--- src/config/api_address.config.ts
+++ src/config/api_address.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const BASE_URL = 'https://dentalitapi.sepasholding.com/api';
 export const API_SEND_OTP = `${BASE_URL}/UnifiedAuth/send-otp`;
 export const API_AUTHENTICATE = `${BASE_URL}/UnifiedAuth/authenticate-merchant`;
@@ -22,29 +21,3 @@ export const API_UPDATE_PROFILE = ` /api/MerchantsCommand/profile`;
 export const API_MERCHANT_MONTH = `${BASE_URL}/MerchantFinancialQuery/Monthly`;
 export const API_MERCHANT_OVERALL = `${BASE_URL}/MerchantFinancialQuery/Overall`;
 export const API_SKILLS = `${BASE_URL}/SkillsQuery`;
-=======
-export const API_SEND_OTP = '/api/UnifiedAuth/send-otp';
-export const API_AUTHENTICATE = '/api/UnifiedAuth/authenticate-merchant';
-export const API_PURCHASE_REQUESTS_COMMAND = '/api/PurchaseRequestsCommand';
-export const API_AUTHENTICATE_ME = '/api/UnifiedAuth/me';
-export const API_MERCHANT = '/api/PurchaseRequestsQuery/merchant/';
-export const API_PURCHASE_REQUESTS_VERIFY =
-  '/api/PurchaseRequestsCommand/verify';
-export const API_ENIAC_QUERY = '/api/EniacQuery/merchant-documents/me';
-export const API_CUSTOMER_QUERY_SIMPLE = '/api/CustomersQuery/simple';
-export const API_MERCHANT_QUERY_SIMPLE = '/api/MerchantsQuery/simple';
-export const API_MERCHANT_PURCHASE_REQUEST =
-  '/api/PurchaseRequestsQuery/my-merchant-purchase-requests';
-export const API_MERCHANT_CONTRACTS_ME = '/api/MerchantContractsQuery/me';
-export const API_MERCHANT_CONTRACTS_ME_POST =
-  '/api/MerchantContractsCommand/me';
-export const API_UPLOAD_PROFILE_IMAGE = '/api/UserProfileImage/upload';
-export const API_GET_PROFILE_IMAGE = '/api/UserProfileImage';
-export const API_PROVINCES_QUERY = '/api/ProvincesQuery';
-export const API_CITIES_QUERY = '/api/CitiesQuery/by-province/';
-export const API_CONTRACT_GET = '/api/MerchantContractsQuery/me';
-export const API_UPDATE_PROFILE = ' /api/MerchantsCommand/profile';
-export const API_MERCHANT_MONTH = '/api/MerchantFinancialQuery/Monthly';
-export const API_MERCHANT_OVERALL = '/api/MerchantFinancialQuery/Overall';
-export const API_SKILLS = '/api/SkillsQuery';
->>>>>>> eb8e29cf585cdbe96dc294bdf92c30da76ccab62
